80163a00462a17b7d8497908f02c162f792cda8a
cem <cemsimseksm@gmail.com>
2022-11-15 18:01:45 +0300

    responsive navbar design completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home"></a>Home
+    </p>
+    <p>
+      <a href="#wgpt3"></a>What is GPT3?
+    </p>
+    <p>
+      <a href="#possibility"></a>Open AI
+    </p>
+    <p>
+      <a href="#features"></a>Case Studies
+    </p>
+    <p>
+      <a href="#blog"></a>Library
+    </p>
+  </>
+);
+
+//BEM -> Block Element Modifier
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              {" "}
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/containers/whatGPT3/WhatGP3.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatGPT3.css";
+
+const WhatGP3 = () => {
+  return <div>WhatGP3</div>;
+};
+
+export default WhatGP3;
