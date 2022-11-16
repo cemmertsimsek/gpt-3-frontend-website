9a838c433e8964d720ebc6b9393a3857a9e4916e
cem <cemsimseksm@gmail.com>
2022-11-16 18:05:38 +0300

    brands are added and styled

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./import";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div className="">
+        <img src={google} alt="google" />
+      </div>
+      <div className="">
+        <img src={slack} alt="slack" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
