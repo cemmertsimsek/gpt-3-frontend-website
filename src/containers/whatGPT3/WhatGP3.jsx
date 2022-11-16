a272d07a52639310f631e4d9af47df90859cbab8
cem <cemsimseksm@gmail.com>
2022-11-16 19:12:54 +0300

    what is gpt3 component styled and finished

--- src/containers/whatGPT3/WhatGP3.jsx
+++ src/containers/whatGPT3/WhatGP3.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./whatGPT3.css";
 
 const WhatGP3 = () => {
-  return <div>WhatGP3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGP3;
